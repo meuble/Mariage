79b5e2848a07cb1075992c97c3a6c46e62d22a06
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2010-03-23 23:10:35 +0100

    Change sweet_words storage from broken sequl + sqlite to a simple unscalable yaml file based storage

--- mariage_claire_et_michel.rb
+++ mariage_claire_et_michel.rb
@@ -1,21 +1,53 @@
 require 'rubygems'
 require 'sinatra'
-require 'sequel'
+require 'YAML'
+require 'YAML/Store'
 
 enable :sessions
 set :clean_trace, false
 
-DB = Sequel.sqlite 'posts.sqlite'
-
-# DB.create_table :posts do
-#  primary_key :id
-#  String :name
-#  String :email
-#  Text :content
-#  Time :created_at
-# end
-
-class Post < Sequel::Model
+class Post
+  COLUMNS = [:id, :name, :email, :content, :created_at]
+  FILENAME = "sweet_words.yml"
+  
+  COLUMNS.each do |f| 
+    define_method(f.to_s) { @attributes[f] }
+  end
+  
+  class << self
+    def all
+      set = YAML::load_file(FILENAME)
+      return [] unless set
+      set.map {|i| self.load i.last}
+    end
+    
+    def load(array)
+      return nil if array.nil? || array.empty?
+      hash = {}
+      array.each_with_index do |value, index|
+        hash[COLUMNS[index]] = value
+      end
+      Post.new(hash)
+    end
+    
+    def new_id
+      Time.now.to_i
+    end
+  end
+  
+  def initialize(attributes = {})
+    @attributes = attributes
+  end
+  
+  def id
+    @attributes[:id] ||= self.class.new_id
+  end
+  
+  def save
+    YAML::Store.new(FILENAME).transaction do | store |
+      store[self.class.name + '_' + self.id.to_s] = COLUMNS.map{|column| @attributes[column]}
+    end
+  end
 end
 
 class Date
